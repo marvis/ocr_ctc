234c0e00471602c60fee7c3565cea784e9a1ea1e
xiaohang <hang_xiao_cn@163.com>
2017-10-15 06:03:03 +0800

    fixed version and move to dev branch

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # imagepath label
 # /ab/cd/image.jpg a:b:c:d 
 
-python main.py --trainlist data/train_list.txt --vallist data/test_list.txt --cuda --adam --lr=0.001
+CUDA_VISIBLE_DEVICES=1 nohup python main.py --trainlist=data/train_list.txt --vallist=data/test_list.txt --cuda --adam --lr=0.001 --niter=1000 --saveInterval=10000 > log.txt &
